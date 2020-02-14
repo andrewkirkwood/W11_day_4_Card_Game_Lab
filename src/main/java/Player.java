cbd592c01b2f230f4a5bc2f16268525f1c7568db
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2020-02-14 16:37:00 +0000

    git test

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,27 +1,27 @@
 import java.util.ArrayList;
 
 public class Player {
-    private Card card;
+    private Hand hand;
 
     public Player(){
-        this.card = null;
+        this.hand = new Hand();
     }
 
-    public void takeACard(Card card){
-        this.card = card;
-    }
+//    public void takeACard(Card card){
+//        this.card = card;
+//    }
+//
+//    public Card giveBackCard(){
+//        Card cardycard = this.card;
+//        this.card = null;
+//        return cardycard;
+//    }
+//
+//    public int getCardValue(){
+//        return this.card.getFaceValue();
+//    }
 
-    public Card giveBackCard(){
-        Card cardycard = this.card;
-        this.card = null;
-        return cardycard;
-    }
-
-    public Card getCard() {
-        return this.card;
-    }
-
-    public int getCardValue(){
-        return this.card.getFaceValue();
+    public Hand getHand() {
+        return this.hand;
     }
 }
--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,27 +1,30 @@
 public class Runner {
     public static void main(String[] args) {
-        Deck deck = new Deck();
-        deck.populateDeck();
-        deck.shuffleDeck();
+//        Deck deck = new Deck();
+//        deck.populateDeck();
+//        deck.shuffleDeck();
+//
+//        Player player1 = new Player();
+//        Player player2 = new Player();
+//
+//        player1.takeACard(deck.dealCard());
+//        player2.takeACard(deck.dealCard());
+//
+//        System.out.println(player1.getCardValue());
+//        System.out.println(player2.getCardValue());
+//
+//        if (player1.getCardValue() > player2.getCardValue()){
+//            System.out.println("player 1 wins");
+//        }
+//        else if (player1.getCardValue() == player2.getCardValue()){
+//            System.out.println("draw");
+//        }
+//        else {
+//            System.out.println("player 2 wins");
+//        }
 
-        Player player1 = new Player();
-        Player player2 = new Player();
 
-        player1.takeACard(deck.dealCard());
-        player2.takeACard(deck.dealCard());
 
-        System.out.println(player1.getCardValue());
-        System.out.println(player2.getCardValue());
-
-        if (player1.getCardValue() > player2.getCardValue()){
-            System.out.println("player 1 wins");
-        }
-        else if (player1.getCardValue() == player2.getCardValue()){
-            System.out.println("draw");
-        }
-        else {
-            System.out.println("player 2 wins");
-        }
 
 //        System.out.println(deck.getCards().get(0).getSuit());
 //        System.out.println(deck.getCards().get(0).getFaceValue());
